3c64f0e7342cea53426f812b0239a4b8b256f66e
shahrukhpunjwani <shahrukhali2520@gmail.com>
2025-12-24 12:21:48 +0500

    changed backend url

--- src/data/baseUrl.js
+++ src/data/baseUrl.js
@@ -1,1 +1,2 @@
-export const BASE_URL = `https://dev-api.app.thegivexchange.com/api`;
+export const BASE_URL = `https://stg-api.app.thegiveXchange.com/api`;
+// export const BASE_URL = `https://dev-api.app.thegiveXchange.com/api`;
