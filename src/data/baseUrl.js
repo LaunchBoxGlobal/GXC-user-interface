1b53fe4f5567bf23327159cff23c30f5d8b26f8e
shahrukhpunjwani <shahrukhali2520@gmail.com>
2026-01-02 15:37:28 +0500

    changed base url

--- src/data/baseUrl.js
+++ src/data/baseUrl.js
@@ -1,5 +1,5 @@
-// this is development base url
+// this is staing base url
 export const BASE_URL = `https://stg-api.app.thegiveXchange.com/api`;
 
-// this is staging base url
+// this is development base url
 // export const BASE_URL = `https://dev-api.app.thegiveXchange.com/api`;
