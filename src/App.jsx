5350df60128a598e6cf8cf3af58e9b294c993a24
shahrukhpunjwani <shahrukhali2520@gmail.com>
2026-01-06 19:33:55 +0500

    changed base url

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
 import { useEffect } from "react";
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
-import { enqueueSnackbar, SnackbarProvider } from "notistack";
-import {
-  listenForMessages,
-  requestNotificationPermission,
-} from "./notifications";
+import { SnackbarProvider } from "notistack";
+import { listenForMessages } from "./notifications";
 
 function App() {
   useEffect(() => {
-    // requestNotificationPermission();
-
     listenForMessages((payload) => {
       const title = payload.notification?.title || "New Notification";
       const body = payload.notification?.body || "";
-      // const icon = payload.notification?.icon || "/logo.png";
 
       new Notification(title, {
         body,
-        // icon,
       });
     });
   }, []);
