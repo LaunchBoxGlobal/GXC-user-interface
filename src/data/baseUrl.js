f6f2e853576ca62c2f8d4c81a6e6266eb45ae12c
shahrukhpunjwani <shahrukhali2520@gmail.com>
2026-03-31 16:16:17 +0500

    changed stripe key

--- src/data/baseUrl.js
+++ src/data/baseUrl.js
@@ -1,5 +1,5 @@
 // this is staing base url
 // export const BASE_URL = `https://stg-api.app.thegiveXchange.com/api`;
 
-// this is development base url
+// this is development base url changed stripe key to testing
 export const BASE_URL = `https://dev-api.app.thegiveXchange.com/api`;
